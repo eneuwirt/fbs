20dc36800281701ed7a967a8a9824199c5aa37ef
Eduard Neuwirt <eduard.neuwirt@googlemail.com>
2011-04-28 17:28:37 +0200

    add spring secuity dependencies

---
+++ fbs-security/src/main/java/com/fbs/security/service/impl/SecurityServiceSpringImpl.java
@@ -0,0 +1,29 @@
+package com.fbs.security.service.impl;
+
+import com.fbs.security.service.SecurityService;
+
+public class SecurityServiceSpringImpl implements SecurityService
+{
+
+    @Override
+    public void login(String userName, String userPassword) throws Exception
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void logout()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public String getTenant(String userName) throws Exception
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}

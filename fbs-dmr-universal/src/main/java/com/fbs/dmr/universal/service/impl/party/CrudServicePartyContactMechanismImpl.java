7ed59f5c463c48e7478dd8d072c8bff932557933
Eduard Neuwirt <eduard.neuwirt@googlemail.com>
2011-06-18 08:52:04 +0200

    contact service

---
+++ fbs-dmr-universal/src/main/java/com/fbs/dmr/universal/service/impl/party/CrudServicePartyContactMechanismImpl.java
@@ -0,0 +1,14 @@
+package com.fbs.dmr.universal.service.impl.party;
+
+import com.fbs.dmr.universal.model.party.PartyContactMechanism;
+import com.fbs.dmr.universal.service.impl.CrudServiceGenericImpl;
+
+public class CrudServicePartyContactMechanismImpl extends CrudServiceGenericImpl<PartyContactMechanism, Integer> 
+{
+
+	public CrudServicePartyContactMechanismImpl()
+    {
+	    super(PartyContactMechanism.class);
+    }
+
+}
---
+++ fbs-dmr-universal/src/main/java/com/fbs/dmr/universal/service/impl/party/CrudServicePartyContactMechanismPurposeImpl.java
@@ -0,0 +1,12 @@
+package com.fbs.dmr.universal.service.impl.party;
+
+import com.fbs.dmr.universal.model.party.PartyContactMechanismPurpose;
+import com.fbs.dmr.universal.service.impl.CrudServiceGenericImpl;
+
+public class CrudServicePartyContactMechanismPurposeImpl  extends CrudServiceGenericImpl<PartyContactMechanismPurpose, Integer>
+{
+	public CrudServicePartyContactMechanismPurposeImpl()
+    {
+	    super(PartyContactMechanismPurpose.class);
+    }
+}
